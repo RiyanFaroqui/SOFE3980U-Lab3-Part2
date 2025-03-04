bdd6b6921350783c41d300050a14247cc03c79c6
riyan <150162900+RiyanFaroqui@users.noreply.github.com>
2025-03-03 19:32:52 -0500

    updated binarycalculator

--- BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryControllerTest.java
+++ BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryControllerTest.java
@@ -16,6 +16,7 @@ import org.junit.runner.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.context.*;
 import org.springframework.boot.test.mock.mockito.*;
+import org.springframework.context.annotation.Description;
 import org.springframework.test.context.junit4.*;
 
 import static org.hamcrest.Matchers.containsString;
@@ -30,31 +31,72 @@ public class BinaryControllerTest {
     @Autowired
     private MockMvc mvc;
 
-   
+    //test for default GET request
     @Test
     public void getDefault() throws Exception {
         this.mvc.perform(get("/"))//.andDo(print())
             .andExpect(status().isOk())
             .andExpect(view().name("calculator"))
-			.andExpect(model().attribute("operand1", ""))
-			.andExpect(model().attribute("operand1Focused", false));
+            .andExpect(model().attribute("operand1", ""))
+            .andExpect(model().attribute("operand1Focused", false));
     }
-	
-	    @Test
+    
+    //test for GET request with parameter
+    @Test
     public void getParameter() throws Exception {
         this.mvc.perform(get("/").param("operand1","111"))
             .andExpect(status().isOk())
             .andExpect(view().name("calculator"))
-			.andExpect(model().attribute("operand1", "111"))
-			.andExpect(model().attribute("operand1Focused", true));
+            .andExpect(model().attribute("operand1", "111"))
+            .andExpect(model().attribute("operand1Focused", true));
     }
-	@Test
-	    public void postParameter() throws Exception {
+
+    //test for POST request with parameters for addition
+    @Test
+    public void postParameter() throws Exception {
         this.mvc.perform(post("/").param("operand1","111").param("operator","+").param("operand2","111"))//.andDo(print())
             .andExpect(status().isOk())
             .andExpect(view().name("result"))
-			.andExpect(model().attribute("result", "1110"))
-			.andExpect(model().attribute("operand1", "111"));
+            .andExpect(model().attribute("result", "1110"))
+            .andExpect(model().attribute("operand1", "111"));
     }
 
+    //test for POST request with parameters for multiplication
+    @Test
+    public void testMultiplication() throws Exception {
+        this.mvc.perform(post("/").param("operand1","111").param("operator","*").param("operand2","101"))//.andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(view().name("result"))
+            .andExpect(model().attribute("result", "100011"));
+    }
+
+    //test for POST request with parameters for OR operation
+    @Test
+    public void testOr() throws Exception {
+        this.mvc.perform(post("/").param("operand1","111").param("operator","|").param("operand2","101"))//.andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(view().name("result"))
+            .andExpect(model().attribute("result", "111"));
+    }
+
+    //test for addition with both operands empty
+    @Test
+    public void testEmptyOperandsAddition() throws Exception {
+        this.mvc.perform(post("/").param("operand1","").param("operator","+").param("operand2",""))
+            .andExpect(status().isOk())
+            .andExpect(view().name("result"))
+            .andExpect(model().attribute("result", "0"));
+    }
+
+    //test for invalid operator
+   /* @Test
+    public void testInvalidOperator() throws Exception {
+        this.mvc.perform(post("/")
+                        .param("operand1", "101")
+                        .param("operator", "%")
+                        .param("operand2", "11"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Error"));
+    }
+    */
 }
